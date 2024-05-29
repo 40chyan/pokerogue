2e10d59936e40476a94e17a23dcea8463b44accd
40chyan <47717431+40chyan@users.noreply.github.com>
2024-05-29 13:33:56 +0800

    Update text.ts

--- src/ui/text.ts
+++ src/ui/text.ts
@@ -44,7 +44,12 @@ const languageSettings: { [key: string]: LanguageSetting } = {
   "es":{},
   "it":{},
   "fr":{},
-  "zh_CN":{},
+  "zh_CN":{	
+           summaryFontSize: "70px",
+           battleInfoFontSize: "65px",
+	         partyFontSize: "50px",
+           tooltipContentFontSize: "50px",
+	         moveInfoFontSize: "50px"},
   "pt_BR":{},
 };
 
